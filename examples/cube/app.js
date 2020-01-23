1797a0b9a391cd19f0fc96220cf44d59c1c17c14
odysz <odysseusj@163.com>
2020-01-24 01:10:29 +0800

    v 0.1.15

--- examples/cube/app.js
+++ examples/cube/app.js
@@ -8,14 +8,14 @@ class App {
 		const xworld = new xv.XWorld(c, window, {
 			camera: {far: 10000} // default 5000
 		});
-		var ecs = xworld.xecs();
+		var ecs = xworld.xecs;
 
 		// FIXME Geometry should already been registered by XObj
 		ecs.registerComponent('Geometry', xv.XComponent.Geometry);
 		// ecs.registerComponent('Visual', xv.XComponent.Visual);
 
 		xworld.addSystem('visu', Business.createCubesys(ecs, {xscene: xworld.xscene}));
-		xworld.update();
+		xworld.startUpdate();
 	}
 }
 
