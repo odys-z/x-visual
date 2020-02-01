25963ffd3cd76a3c8004accad28eba7b460938dc
odys zhou <odysseusj@163.com>
2020-02-01 13:02:54 +0800

    .

---
+++ test/gltf-mesh.case.js
@@ -0,0 +1,18 @@
+
+/** @module xv.test.assets */
+
+// import chai from 'chai'
+import { chai, expect, assert } from 'chai'
+import chaiStats from 'chai-stats'
+
+import AssetKeepr from '../lib/xutils/assetkeepr'
+
+const {performance} = require('perf_hooks');
+
+describe('case: [gltf] load mesh', function() {
+	before(() => {
+	});
+
+	it('simple.gltf', function() {
+	});
+});
