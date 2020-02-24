17569668692ac8f8d717266b6d894ac6da9d6945
odysz <odysseusj@163.com>
2020-02-24 23:57:34 +0800

    .

--- test/api-script-affines.case.js
+++ test/api-script-affines.case.js
@@ -25,37 +25,37 @@ describe('case: [affine] orbit combine', function() {
     this.timeout(100000);
     x.log = 4;
 
-    // it('affine array: trans, rotate, -trans', async function() {
-    //     const xworld = new XWorld(undefined, 'window', {});
-    //     const ecs = xworld.xecs;
-    //
-    //     var cube = ecs.createEntity({
-    //         id: 'affine-arr',
-    //         Obj3: { geom: Obj3Type.BOX,
-    //                 box: [200, 120, 80],
-    //                 mesh: undefined },
-    //         Visual:{vtype: AssetType.mesh,
-    //                 asset: null },
-    //         ModelSeqs: { script: [[
-    //               { mtype: AnimType.ORBIT,
-    //                 paras: {start: 0,        // auto start,
-    //                         duration: 0.2,
-    //                         axis: [0, 1, 0],
-    //                         pivot: [120, 0, 0],
-    //                         deg: [0, 60],
-    //                         ease: null} }
-    //               ]] },
-    //         CmpTweens: {}
-    //     });
-    //
-    //     xworld.startUpdate();
-    //         await sleep(500);
-    //         xworld.update();
-    //         // equilateral triangle
-    //         var wp = cube.Obj3.mesh.getWorldPosition();
-    //         var len = new vec3(wp).length();
-    //         assert.closeTo(len, 120, 0.5); // 120 = pivot.len
-    // });
+    it('affine array: trans, rotate, -trans', async function() {
+        const xworld = new XWorld(undefined, 'window', {});
+        const ecs = xworld.xecs;
+
+        var cube = ecs.createEntity({
+            id: 'affine-arr',
+            Obj3: { geom: Obj3Type.BOX,
+                    box: [200, 120, 80],
+                    mesh: undefined },
+            Visual:{vtype: AssetType.mesh,
+                    asset: null },
+            ModelSeqs: { script: [[
+                  { mtype: AnimType.ORBIT,
+                    paras: {start: 0,        // auto start,
+                            duration: 0.2,
+                            axis: [0, 1, 0],
+                            pivot: [120, 0, 0],
+                            deg: [0, 60],
+                            ease: null} }
+                  ]] },
+            CmpTweens: {}
+        });
+
+        xworld.startUpdate();
+            await sleep(500);
+            xworld.update();
+            // equilateral triangle
+            var wp = cube.Obj3.mesh.getWorldPosition();
+            var len = new vec3(wp).length();
+            assert.closeTo(len, 120, 0.5); // 120 = pivot.len
+    });
 
     it('affine combination: orbit {pivot: [120, 0, 0], axis: [0, 1, 0]}', async function() {
         const xworld = new XWorld(undefined, 'window', {});
@@ -105,48 +105,48 @@ describe('case: [affine] orbit combine', function() {
                         'orbited back to start v.s transform combined');
     });
 
-    // it('affine combination: orbit + roate x', async function() {
-    //     const xworld = new XWorld(undefined, 'window', {});
-    //     const ecs = xworld.xecs;
-    //
-    //     var cube = ecs.createEntity({
-    //         id: 'orbit-rotatex',
-    //         Obj3: { geom: Obj3Type.BOX,
-    //                 box: [200, 120, 80],
-    //                 mesh: undefined },
-    //         Visual:{vtype: AssetType.mesh,
-    //                 asset: null },
-    //         ModelSeqs: { script: [
-    //              [{ mtype: AnimType.ORBIT,
-    //                 paras: {start: Infinity,
-    //                         duration: 0.2,
-    //                         axis: [0, 1, 0],
-    //                         pivot: [120, 0, 0],
-    //                         deg: [0, 180],
-    //                         ease: null} }],
-    //              [{ mtype: AnimType.ROTATEX,
-    //                 paras: {start: Infinity,
-    //                         duration: 0.4,
-    //                         deg: [0, 60],
-    //                         ease: null} } ],
-    //             ] },
-    //         CmpTweens: {}
-    //     });
-    //
-    //     xworld.startUpdate();
-    //         cube.CmpTweens.startCmds.push(0);
-    //         // cube.CmpTweens.startCmds.push(1); FIXME  FIXME  FIXME  FIXME  FIXME
-    //         xworld.update();
-    //         await sleep(300);
-    //         xworld.update();
-    //         await sleep(200);
-    //         xworld.update();
-    //         var mat = cube.Obj3.mesh.matrix;
-    //         var mt4 = new mat4()
-    //                     .translate(-120, 0, 0)
-    //                     .rotate(radian(180), 0, 1, 0)
-    //                     .translate(120, 0, 0)
-    //                     // .rotate(radian(60), 1, 0, 0);
-    //         assert.isTrue(mt4.transpose().eq(new mat4(mat)), 'orbit + rotatex v.s transform combined');
-    // });
+    it('affine combination: orbit + roate x', async function() {
+        const xworld = new XWorld(undefined, 'window', {});
+        const ecs = xworld.xecs;
+
+        var cube = ecs.createEntity({
+            id: 'orbit-rotatex',
+            Obj3: { geom: Obj3Type.BOX,
+                    box: [200, 120, 80],
+                    mesh: undefined },
+            Visual:{vtype: AssetType.mesh,
+                    asset: null },
+            ModelSeqs: { script: [
+                 [{ mtype: AnimType.ORBIT,
+                    paras: {start: Infinity,
+                            duration: 0.2,
+                            axis: [0, 1, 0],
+                            pivot: [120, 0, 0],
+                            deg: [0, 180],
+                            ease: null} }],
+                 [{ mtype: AnimType.ROTATEX,
+                    paras: {start: Infinity,
+                            duration: 0.4,
+                            deg: [0, 60],
+                            ease: null} } ],
+                ] },
+            CmpTweens: {}
+        });
+
+        xworld.startUpdate();
+            cube.CmpTweens.startCmds.push(0);
+            // cube.CmpTweens.startCmds.push(1); FIXME  FIXME  FIXME  FIXME  FIXME
+            xworld.update();
+            await sleep(300);
+            xworld.update();
+            await sleep(200);
+            xworld.update();
+            var mat = cube.Obj3.mesh.matrix;
+            var mt4 = new mat4()
+                        .translate(-120, 0, 0)
+                        .rotate(radian(180), 0, 1, 0)
+                        .translate(120, 0, 0)
+                        // .rotate(radian(60), 1, 0, 0);
+            assert.isTrue(mt4.transpose().eq(new mat4(mat)), 'orbit + rotatex v.s transform combined');
+    });
 });
