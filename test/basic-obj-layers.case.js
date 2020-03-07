15b1e52b408767ce8c0b60bebb25bc2e00275a4e
odysz <odysseusj@163.com>
2020-03-07 23:59:39 +0800

    .

--- test/basic-obj-layers.case.js
+++ test/basic-obj-layers.case.js
@@ -4,24 +4,71 @@ import chai from 'chai'
 import { expect, assert } from 'chai'
 import chaiStats from 'chai-stats'
 
-const {performance} = require('perf_hooks');
-
+import * as THREE from 'three'
 import * as ECS from '../packages/ecs-js/index';
 import XWorld from '../lib/xapp/xworld.js'
 import {x} from '../lib/xapp/xworld.js'
 import {AssetType} from '../lib/component/visual'
 import {Obj3Type} from '../lib/component/obj3'
+import * as Composers from '../lib/component/ext/effects'
+import {Layers, LayerFilter} from '../lib/xmath/layer'
+
+const {performance} = require('perf_hooks');
+////////////////////////////////////////////////
+// installation failed while needle downloading
+// var Canvas = require("canvas");
+// Use the China canvas
+////////////////////////////////////////////////
 
 global.performance = performance;
 
 describe('case: Layers', function() {
-	before(function() { });
+	let canvas;
+	before(function() {
+		THREE.Layers.prototype.visible = function (mask) {
+			return ( this.mask & mask ) !== 0;
+		}
+		THREE.Layers.prototype.occlude = function (mask) {
+			return this.test(~mask);
+		}
+		canvas = {
+			width: 2,
+			height: 1,
+			style: {},
+			getContext: ()=> { return new CanvasRenderingContext2D(); },
+			addEventListener: function(){},
+			removeEventListener: function(){},
+		}
+	});
 
 	it('occluded', function() {
-		var xworld = new XWorld(undefined, 'window', {});
+		var xworld = new XWorld(undefined, 'window', {pathEffects: true});
 		var ecs = xworld.xecs;
-		var cube = ecs.createEntity({
-			id: 'bloom-0',
+		xworld.registerComponents(ecs, Composers);
+		x.renderer = new THREE.WebGLRenderer({canvas, alpha: true});
+
+		var v1 = ecs.createEntity({
+			id: 'v1',
+			Obj3: { geom: Obj3Type.BOX,
+					box: [200, 120, 80],
+					mesh: undefined },
+			Visual:{vtype: AssetType.mesh,
+					asset: null },
+			FlowingPath: {}
+		});
+
+		var o1 = ecs.createEntity({
+			id: 'o1',
+			Obj3: { geom: Obj3Type.BOX,
+					box: [20, 120, 80],
+					mesh: undefined },
+			Visual:{vtype: AssetType.mesh,
+					asset: null },
+			Occluder: {occlude: {FlowingPath: true}}
+		});
+
+		var inv = ecs.createEntity({
+			id: 'invisible',
 			Obj3: { geom: Obj3Type.BOX,
 					box: [200, 120, 80],
 					mesh: undefined },
@@ -29,17 +76,17 @@ describe('case: Layers', function() {
 					asset: null },
 		});
 
-		cube = ecs.createEntity({
-			id: 'occluded-1',
-			Obj3: { geom: Obj3Type.BOX,
-					box: [20, 120, 80],
-					mesh: undefined },
-			Visual:{vtype: AssetType.mesh,
-					asset: null },
-		});
+		var camera = new THREE.Layers().push(LayerFilter.FLOWING_PATH);
 
 		xworld.startUpdate();
-		assert.isTrue(x.pathEffects);
+		debugger
+		assert.isTrue(camera.visible(v1.Obj3.layers), 'v1 visible');
+		assert.isFalse(camera.occlude(v1.Obj3.occluding), 'v1 occlude');
+		assert.isTrue(camera.visible(o1.Obj3.layers), 'o1 visible');
+		assert.isTrue(camera.occlude(o1.Obj3.occluding), 'o1 occlude');
+
+		assert.isFalse(camera.visible(inv.Obj3.layers), 'inv visible');
+		assert.isFalse(camera.occlude(inv.Obj3.occluding), 'inv occlude');
 
 	});
 });
